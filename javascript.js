508f298bf7f516a04954d7c314562732d861d71c
Stephen <121040221+stephenstrange-blip@users.noreply.github.com>
2024-09-28 22:09:09 +0800

    build: Create functions for playing a round and the whole game

--- javascript.js
+++ javascript.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 
 /*  if random_num is less than 5, choose rock
     if random_num is greater than 5, choose paper,
@@ -11,15 +8,15 @@ function getComputerChoice() {
     let random_num = Math.floor(Math.random() *10);
 
     if (random_num < 5){
-        console.log("Rock", random_num);
+        // console.log("Rock", random_num);
         return "Rock";
     }
     else if (random_num > 5){
-        console.log("Paper", random_num);
+        //console.log("Paper", random_num);
         return "Paper";
     }
     else {
-        console.log("Scissors", random_num);
+        //console.log("Scissors", random_num);
         return "Scissors";
     }
 }
@@ -34,11 +31,11 @@ function getHumanChoice() {
             // break;
         }
         else if (user_input.toLowerCase().trim() === "paper") {
-            return "Paper"
+            return "Paper";
             // break;
         }
         else if (user_input.toLowerCase().trim() === "scissors") {
-            return "Scissors"
+            return "Scissors";
             // break;
         }
         else {
@@ -47,8 +44,108 @@ function getHumanChoice() {
         }
     }
 }
-getHumanChoice();
-function playRound(humanChoice, computerChoice) {
-    return;
+
+/*
+    If human plays rock
+        if computer plays rock, plus 1 to both
+        if computer play paper, plus 1 to computer
+        if computer play scissors, plus 1 to human
+    if human plays paper
+        if computer plays rock, plus 1 to human
+        if computer play paper, plus 1 to both
+        if computer play scissors, plus 1 to computer
+    if human plays scissors
+        if computer plays rock, plus 1 to computer
+        if computer play paper, plus 1 to human
+        if computer play scissors, plus 1 to both
+*/
+
+function playRound(humanChoice, computerChoice, humanScore, computerScore) {
+    if (humanChoice === "Rock") {
+        if (computerChoice === "Rock"){
+            humanScore++;
+            computerScore++;
+            tie();
+            showScore(humanScore, computerScore);
+        }
+        else if (computerChoice === "Paper") {
+            computerScore++;
+            lose(humanChoice, computerChoice);
+            showScore(humanScore, computerScore);
+        }
+        else {
+            humanScore++;
+            win(humanChoice, computerChoice);
+            showScore(humanScore, computerScore);
+        }
+    }
+    if (humanChoice === "Paper") {
+        if (computerChoice === "Rock"){
+            humanScore++;
+            win(humanChoice, computerChoice);
+            showScore(humanScore, computerScore);
+        }
+        else if (computerChoice === "Paper") {
+            computerScore++;
+            humanScore++;
+            tie();
+            showScore(humanScore, computerScore);
+        }
+        else {
+            computerScore++;
+            lose(humanChoice, computerChoice);
+            showScore(humanScore, computerScore);
+        }
+    }
+    if (humanChoice === "Scissors") {
+        if (computerChoice === "Rock"){
+            computerScore++;
+            lose(humanChoice, computerChoice);
+            showScore(humanScore, computerScore);
+        }
+        else if (computerChoice === "Paper") {
+            humanScore++;
+            win(humanChoice, computerChoice);
+            showScore(humanScore, computerScore);
+        }
+        else {
+            humanScore++;
+            computerScore++;
+            tie();
+            showScore(humanScore, computerScore);
+        }
+    }
+    return [humanScore, computerScore];
 }
 
+
+
+/*  inside playGame()
+        declare score
+        call function 5 times
+        display score
+*/
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+    for (let i = 1; i <= 5; i++) {
+        let result = playRound(getHumanChoice(), getComputerChoice(), humanScore, computerScore)
+        humanScore = result[0];
+        computerScore = result[1];
+    }
+}
+
+playGame();
+
+function tie(){
+    console.log("Its a Tie!");
+}
+function lose(human,computer){
+    console.log(`You lose! ${computer} beats ${human}.`);
+}
+function win(human, computer){
+    console.log(`You win! ${human} beats ${computer}.`);
+}
+function showScore(human, computer) {
+    console.log(`You: ${human} , Computer: ${computer}.`);
+}
